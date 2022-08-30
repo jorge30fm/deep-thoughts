7ff0ceebe33ea7d336f9df665895a7847ef75160
Jorge Felix <jorge30fm@gmail.com>
2022-08-30 16:48:19 -0400

    re-created project files to fix dependency issues

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,29 +1,27 @@
-const { User, Thought } = require("../models");
+const { User, Thought } = require('../models');
 
 const resolvers = {
-	Query: {
-		thoughts: async (parent, { username }) => {
-			const params = username ? { username } : {};
-			return Thought.find(params).sort({ createdAt: -1 });
-		},
-		thought: async (parent, { _id }) => {
-			return Thought.findOne({ _id });
-		},
-        //get all users
-        users: async() => {
-            return User.find()
-                .select('-__v -password') //'-' omit this information
-                .populate('friends')
-                .populate('thoughts');
-        },
-        //get a user by username
-        user: async (parent, {username }) => {
-            return User.findOne({username})
-            .select('-__v -password')
-            .populate('friends'
-            .populate('thoughts'));
-        }
-	},
+  Query: {
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('friends');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    }
+  }
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,38 +1,37 @@
-//import the  gqltagged template function
-const { gql } = require("apollo-server-express");
-//create our type Defs
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-	type Thought {
-		_id: ID
-        thoughtText: String
-        createdAt: String
-        username: String
-        reactionCount: Int
-        reactions: [Reaction]
-	}
-    type Reaction {
-        _id: ID
-        reactionBody: String
-        createdAt: String
-        username: String
-    }
-    type User {
-        _id: ID
-        username: String
-        email: String
-        friendCount: Int
-        thoughts: [Thought]
-        friends: [User]
-    }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
 
-	type Query {
-        users: [User]
-        user(username: String!): User
-		thoughts(username: String): [Thought]
-        thought(_id: ID!): Thought
-	}
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
 `;
 
-//export the typeDefs
 module.exports = typeDefs;
