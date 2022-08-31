e11ff0d74258d484e82c37e768d9a098ee371146
Jorge Felix <jorge30fm@gmail.com>
2022-08-31 12:02:53 -0400

    add mutations for thoughts, friends, and reactions

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const {authMiddleware} = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -7,19 +8,20 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
- 
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-  
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
@@ -27,6 +29,6 @@ const startApolloServer = async (typeDefs, resolvers) => {
     })
   })
   };
-  
+
   // Call the async function to start the server
   startApolloServer(typeDefs, resolvers);
